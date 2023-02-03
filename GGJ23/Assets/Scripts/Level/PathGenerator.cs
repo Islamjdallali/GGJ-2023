0f2048954d15ac5c1a82d55cce749a5bd65f2064
IslamDallali <islamjdallali@gmail.com>
2023-02-03 13:42:02 +0000

    added basic path generator

---
+++ GGJ23/Assets/Scripts/Level/PathGenerator.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject parent;
+    [SerializeField] private GameObject[] paths;
+    [SerializeField] private float timeTillDeath;
+    [SerializeField] private Vector3 offset;
+
+    [SerializeField] private int randNo;
+
+    private void Start()
+    {
+        randNo = Random.Range(0, paths.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeTillDeath -= Time.deltaTime;
+        if (timeTillDeath <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Instantiate(paths[randNo], transform.position + offset, Quaternion.identity);
+    }
+}
